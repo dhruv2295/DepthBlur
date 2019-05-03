48ac4e5783ea7cc03a369e6e8a44f34a93cea936
Dhruv <dhruv2295@gmail.com>
2019-05-03 18:22:29 +0530

    gradle

--- app/src/main/java/com/firebot/dhruv/tensorflow/MainActivity.java
+++ app/src/main/java/com/firebot/dhruv/tensorflow/MainActivity.java
@@ -1,6 +1,9 @@
 package com.firebot.dhruv.tensorflow;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
@@ -9,13 +12,20 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import org.tensorflow.lite.Interpreter;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -47,42 +57,70 @@ public class MainActivity extends AppCompatActivity {
 
 	private int[][] segmentMap;
 	ImageView destImage;
+	int w, h;
+	ImageView sourceImage;
+
+	Bitmap scaledDown;
+	Bitmap scaleExtend;
 
 //	LABEL_NAMES =
-//			'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-//			'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
-//			'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
+//			'0:background', '1:aeroplane', '2:bicycle', '3:bird', '4:boat', '5:bottle', '6:bus',
+//			'7:car', '8:cat', '9:chair', '10:cow', '11:diningtable', '12:dog', '13:horse', '14:motorbike',
+//			'15:person', '16:pottedplant', '17:sheep', '18:sofa', '19:train', '20:tv'
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-
-		Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.joey);
-
-		ImageView sourceImage = findViewById(R.id.source);
-
 		destImage = findViewById(R.id.segment);
-
-		sourceImage.setImageBitmap(b);
-
 		imgData = ByteBuffer.allocateDirect(1 * inputSize * inputSize * 3 * numBytesPerChannel);
 		imgData.order(ByteOrder.nativeOrder());
 		labelProbArray = new float[1][inputSize][inputSize][21];
 
 		intValues = new int[inputSize * inputSize];
 		segmentMap = new int[inputSize][inputSize];
-		try {
-			tflite = new Interpreter(loadModelFile(this));
 
 
-			convertBitmapToByteBuffer(Bitmap.createScaledBitmap(b, inputSize, inputSize, false));
+		Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.joey_small);
+
+		File sdCard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);// or DIRECTORY_PICTURES
+		File media = new File(sdCard.getAbsolutePath() + "/J.jpg");
+//		File media = new File(sdCard.getAbsolutePath() + "/self.jpg");
+//		File media = new File(sdCard.getAbsolutePath()+"/Before.jpeg");
+//		File media = new File(sdCard.getAbsolutePath()+"/sample_image.jpeg");
+
+		sourceImage = findViewById(R.id.source);
+
+		Glide.with(this).asBitmap().load(media).into(new SimpleTarget<Bitmap>() {
+			@Override
+			public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+				sourceImage.setImageBitmap(resource);
+				w = resource.getWidth();
+				h = resource.getHeight();
+
+				try {
+					tflite = new Interpreter(loadModelFile(MainActivity.this));
+
+					float resizeRatio = (float) inputSize / Math.max(w, h);
+					int rw = Math.round(w * resizeRatio);
+					int rh = Math.round(h * resizeRatio);
+
+
+					scaledDown = Bitmap.createScaledBitmap(resource, rw, rh, true);
+					scaleExtend = Utils.extendBitmap(scaledDown, inputSize, inputSize, Color.BLACK);
+
+					sourceImage.setImageBitmap(scaleExtend);
+					Log.d("Data", rw + ":" + rh);
+
+					convertBitmapToByteBuffer(scaleExtend);
 //			convertBitmapToByteBuffer(b);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
 
 	}
@@ -127,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
 		tflite.run(imgData, labelProbArray);
 
 
-		Utils.fillZeroes(segmentMap);
+//		Utils.fillZeroes(segmentMap);
 		for (int i = 0; i < inputSize; i++)
 			for (int j = 0; j < inputSize; j++) {
 				segmentMap[i][j] = getIndexOfLargest(labelProbArray[0][i][j]);
@@ -141,21 +179,35 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	public void drawBitmap() {
-		Bitmap imageBitmap = Bitmap.createBitmap(inputSize,
-				inputSize, Bitmap.Config.ARGB_8888);
-		Canvas canvas = new Canvas(imageBitmap);
+		Bitmap mask = Bitmap.createBitmap(inputSize, inputSize, Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(mask);
 
 		Paint p = new Paint();
-		p.setColor(Color.BLUE);
+		p.setColor(Color.RED);
 
 		for (int i = 0; i < inputSize; i++)
 			for (int j = 0; j < inputSize; j++) {
 				if (segmentMap[i][j] != 0)
+				{
+//					p.setColor(scaleExtend.getPixel(j,i));
 					canvas.drawPoint(j, i, p);
+				}
+				else
+				{
+//					p.setColor(blurred.getPixel(j,i));
+//					canvas.drawPoint(j, i, p);
+
+				}
 			}
 
+		Bitmap scaleUp = Bitmap.createScaledBitmap(mask, w, h, false);
 
-		destImage.setImageBitmap(imageBitmap);
+		ImageView original = findViewById(R.id.original);
+		original.setImageBitmap(scaledDown);
+		destImage.setImageBitmap(mask);
+		destImage.setAlpha(0.5f);
+//		destImage.setImageBitmap(scaleUp);
+
 
 	}
 
--- app/src/main/java/com/firebot/dhruv/tensorflow/Utils.java
+++ app/src/main/java/com/firebot/dhruv/tensorflow/Utils.java
@@ -1,32 +1,38 @@
 package com.firebot.dhruv.tensorflow;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.util.Log;
 
+import androidx.exifinterface.media.ExifInterface;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class Utils {
 
-	public static int getIndexOfLargest( float[] array )
-	{
-		if ( array == null || array.length == 0 ) return -1; // null or empty
+	public static int getIndexOfLargest(float[] array) {
+		if (array == null || array.length == 0) return -1; // null or empty
 
 		int largest = 0;
-		for ( int i = 1; i < array.length; i++ )
-		{
-			if ( array[i] > array[largest] ) largest = i;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[largest]) largest = i;
 		}
 		return largest; // position of the first largest found
 	}
 
 
-	public static void duplicateCount(int[][] data)
-	{
+	public static void duplicateCount(int[][] data) {
 		HashMap<Integer, Integer> repetitions = new HashMap<>();
 
 		for (int i = 0; i < 257; ++i) {
-			for(int j=0; j<257; ++j) {
+			for (int j = 0; j < 257; ++j) {
 				int item = data[i][j];
 
 				if (repetitions.containsKey(item))
@@ -59,7 +65,7 @@ public class Utils {
 			sb.insert(0, "There are ");
 		}
 
-		Log.d("Data",sb.toString());
+		Log.d("Data", sb.toString());
 
 	}
 
@@ -72,6 +78,84 @@ public class Utils {
 		int r;
 		for (r = 0; r < array.length; r++) {
 			Arrays.fill(array[r], 0);
+		}
+	}
+
+	public static Bitmap extendBitmap(Bitmap origin, int destW, int destH, int backgroundColor) {
+		if (origin != null && destW > 0 && destH > 0) {
+			int var4 = origin.getWidth();
+			int var5 = origin.getHeight();
+			if (destW >= var4 && destH >= var5) {
+				Log.d("Data", "origin =, dest =" + new Object[]{var4, var5, destW, destH});
+				Bitmap var6 = Bitmap.createBitmap(destW, destH, Bitmap.Config.ARGB_8888);
+				Canvas var7 = new Canvas(var6);
+				Paint var8 = new Paint(1);
+				var7.drawColor(backgroundColor);
+				int var9 = (int) Math.round((double) (destW - var4) / 2.0D);
+				int var10 = (int) Math.round((double) (destH - var5) / 2.0D);
+				Log.d("Data", "xOffset =, yOffset = " + new Object[]{var9, var10});
+				var7.drawBitmap(origin, (float) var9, (float) var10, var8);
+				return var6;
+			} else {
+				return origin;
+			}
+		} else {
+			return origin;
+		}
+	}
+
+
+	public static Bitmap rotateBitmap(Bitmap bitmap, InputStream path) {
+
+		ExifInterface exif = null;
+		try {
+			exif = new ExifInterface(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+				ExifInterface.ORIENTATION_UNDEFINED);
+
+		Log.d("Utils", "Orientation:"+ orientation);
+		Matrix matrix = new Matrix();
+		switch (orientation) {
+			case ExifInterface.ORIENTATION_NORMAL:
+				return bitmap;
+			case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+				matrix.setScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_180:
+				matrix.setRotate(180);
+				break;
+			case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+				matrix.setRotate(180);
+				matrix.postScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_TRANSPOSE:
+				matrix.setRotate(90);
+				matrix.postScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_90:
+				matrix.setRotate(90);
+				break;
+			case ExifInterface.ORIENTATION_TRANSVERSE:
+				matrix.setRotate(-90);
+				matrix.postScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_270:
+				matrix.setRotate(-90);
+				break;
+			default:
+				return bitmap;
+		}
+		try {
+			Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+			bitmap.recycle();
+			return bmRotated;
+		} catch (OutOfMemoryError e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
